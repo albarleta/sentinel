d9f5601cfc0b5ca6099478f81aa62a8ad063cb43
Steven Gatchalian <steven.gatch18@gmail.com>
2024-12-08 21:19:09 +0000

    Revert "Merge branch 'dev-steven' into 'main'"
    This reverts merge request !30

--- server/routes/notificationRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import NotificationController from "../controllers/NotificationController.js";
-import { authenticate } from "../middlewares/authMiddleware.js";
-
-const router = Router();
-router.get("/:userId", NotificationController.getNotifications);
-router.patch("/:id", NotificationController.updateNotification);
-
-export default router;
